a3f4300e21a98f205fdac7901c6520b81e52df89
tobyy26 <toby.yarrington26@gmail.com>
2026-07-15 10:52:13 -0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'nearby-shows-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
